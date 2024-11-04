29169fb5e1aa0f9d5d1f03a7e4ada659911c74bc
wooshdude <parker@cline.cc>
2024-11-03 17:00:23 -0800

    added search functionality to grid manager

--- scripts/globals/grid_manager.gd
+++ scripts/globals/grid_manager.gd
@@ -17,6 +17,31 @@ func check_valid_position(position: Vector2):
 		return false
 	return true
 
+func get_object_at_position(position: Vector2i) -> GridComponent:
+	if objects.has(position):
+		return objects[position]
+	return null
+
+func get_neighbors(position: Vector2i) -> Array[GridComponent]:
+	var neighbors: Array[GridComponent]
+	var positions: Array[Vector2i] = [
+		# up
+		position + Vector2i(0,-1),
+		# down
+		position + Vector2i(0,1),
+		# left
+		position + Vector2i(-1,0),
+		# right
+		position + Vector2i(1,0)
+	]
+
+	for pos in positions:
+		var n = get_object_at_position(pos)
+		if n:
+			neighbors.append(n)
+
+	return neighbors
+
 func register_object(obj: GridComponent):
 	if not obj in objects.values():
 		objects[obj.Position] = obj
@@ -25,8 +50,8 @@ func remove_object(obj: GridComponent):
 	if obj in objects.values():
 		objects.erase(obj.Position)
 
-func world_to_cell(position: Vector2):
+func world_to_cell(position: Vector2) -> Vector2i:
 	return world_map.local_to_map(position)
 
-func cell_to_world(position: Vector2i):
+func cell_to_world(position: Vector2i) -> Vector2:
 	return world_map.map_to_local(position)
