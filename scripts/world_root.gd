89b23df1656e44ba936897279792be3eb8b33e17
wooshdude <parker@cline.cc>
2024-11-10 12:09:01 -0800

    polish grid behaviors

--- scripts/world_root.gd
+++ scripts/world_root.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node
 class_name WorldRoot
 
@@ -5,6 +6,11 @@ class_name WorldRoot
 @export var world3d: Node3D
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		if self.get_child_count() == 0:
+			add_child(world2d)
+			add_child(world3d)
+
 	for child in self.get_children(false):
 		if child is Node2D:
 			world2d = child
